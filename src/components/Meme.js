b9d90a96d874a00ec669ccadbf0e8ad9632ad125
Tony <TonyLin3591@gmail.com>
2022-07-16 12:59:43 -0400

    deploy

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -5,6 +5,9 @@ import memesData from "../data.js";
 export default function Meme()
 {
      const[image, setUrl] = React.useState("http://i.imgflip.com/1bij.jpg")
+     const[formData,setFormData] = React.useState(
+          {topText:"" , botText:""}
+     )
      
 
      function GetMemeImage(){
@@ -15,18 +18,33 @@ export default function Meme()
           setUrl(url);
 
      }
+
+     function handleChange(event){
+          setFormData(prevFormData => {
+               return {
+                   ...prevFormData,
+                   [event.target.name]: event.target.value
+               }
+          })
+
+     }
+
+
+
         
      return(
           <div className="memebox">
                <div className="inputDiv">
-                    <input className="input1" placeholder="    Top Text"></input>
-                    <input className="input2" placeholder="    Bottom Text"></input>
+                    <form>
+                         <input className="input1" placeholder="    Top Text" onChange={handleChange} name="topText" value={formData.topText}></input>
+                         <input className="input2" placeholder="    Bottom Text" onChange={handleChange} name="botText" value={formData.botText}></input>
+                    </form>
                </div>
                     <button className="generateButton" onClick={GetMemeImage}> Get a new meme image 🖼</button>
                <div>
-                    <p className="toptext"> </p>
+                    <p className="topText"> {formData.topText} </p>
                     <img className="memeImg" src={image}></img>
-                    <p className="bottext"> </p>
+                    <p className="botText"> {formData.botText}</p>
                </div>
           </div>
 
