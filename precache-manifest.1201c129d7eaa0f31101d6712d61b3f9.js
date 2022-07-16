36f59ef0ef072a1f46af3ec6a05c71270f35e5d1
Tony <TonyLin3591@gmail.com>
2022-07-16 13:02:11 -0400

    Updates

---
+++ precache-manifest.1201c129d7eaa0f31101d6712d61b3f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb8498a42a616b51408b",
+    "url": "/meme-gen/static/css/main.fd2aaef4.chunk.css"
+  },
+  {
+    "revision": "fb8498a42a616b51408b",
+    "url": "/meme-gen/static/js/main.1994d688.chunk.js"
+  },
+  {
+    "revision": "61d14f92e7209c6eb89e",
+    "url": "/meme-gen/static/js/runtime~main.83457914.js"
+  },
+  {
+    "revision": "4b9a3f8b5ba7d2f77463",
+    "url": "/meme-gen/static/js/2.1c6cdf4e.chunk.js"
+  },
+  {
+    "revision": "c0c98a6c240aa4e0d888",
+    "url": "/meme-gen/static/js/3.68b36b5f.chunk.js"
+  },
+  {
+    "revision": "16ea36d12a62aabe0c6c6acc4c3a7a76",
+    "url": "/meme-gen/static/media/trollface.16ea36d1.png"
+  },
+  {
+    "revision": "117ff8c83563b98f3f2e39affd76e15f",
+    "url": "/meme-gen/index.html"
+  }
+];
