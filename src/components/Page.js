6d0e4495798b3bdf130123af66583f32a947bbfc
Tony <TonyLin3591@gmail.com>
2022-07-19 21:38:59 -0400

    deploy

--- src/components/Page.js
+++ src/components/Page.js
@@ -4,6 +4,11 @@ import Meme from '../components/Meme.js';
 
 export default function Page()
 {
+
+     const pokemon = ["Bulbasaur", "Charmander", "Squirtle"]
+
+
+     const paragraphs = pokemon.map(mon => `<p>${mon}</p>`)
     /* const dataElements = data.map( element => {
           return <Card title={element.title} openSpots={element.openSpots} review={element.stats.reviewCount} location={element.location}price={element.price} rating={element.stats.rating} coverImg={element.coverImg} />
         })*/
